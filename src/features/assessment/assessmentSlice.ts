cda93b6a2667740648f3cd7e93956e9cbfaa7afd
Hamsa Harcourt <hamsaharcourt@gmail.com>
2023-09-20 01:26:17 +0100

    fix assessment slice

--- src/features/assessment/assessmentSlice.ts
+++ src/features/assessment/assessmentSlice.ts
@@ -1,33 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface GeoState {
-    questions: [] | null;
+  questions: [] | null;
 }
 
 const initialState: GeoState = {
-    questions: null
+  questions: null,
 };
 
 const assessmentSlice = createSlice({
-    name: "assessment",
-    initialState,
-    reducers: {
-        addQuestion: (state, action: PayloadAction<{ questions: []; }>) => {
-            if (state.questions){
-                state.questions = state.questions.push(action.payload.questions)
-            } else {
-                state.questions = [...action.payload.questions]
-                
-            }
-        },
-
-
-        resetQuestions: (state) => {
-             state.questions = null
-        },
-
-        
+  name: "assessment",
+  initialState,
+  reducers: {
+    addQuestion: (state, action: PayloadAction<{ questions: [] }>) => {
+      state.questions = state.questions
+        ? [...state.questions, ...action.payload.questions]
+        : [...action.payload.questions];
     },
+
+    resetQuestions: (state) => {
+      state.questions = null;
+    },
+  },
 });
 
 export const { addQuestion, resetQuestions } = assessmentSlice.actions;
