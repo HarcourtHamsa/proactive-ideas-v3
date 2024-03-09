c2b2f7735dc29adec1c3c67a3ec43b3b717e6bff
User <user@Users-MacBook-Pro.local>
2024-03-09 15:45:02 +0100

    fixes loader issue

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import Logo from './Logo'
+import logo from "@/assets/idea.webp"
+import Image from 'next/image'
 
 function Loader() {
     return (
         <div className='w-screen h-screen flex justify-center items-center  bg-[#FAF7ED]'>
-            <div className='bg-red-100 lg:w-[300px] w-[60%] text-center relative'>
-                
-                <span className=" -translate-x-[120%] absolute lg:left-[48%] left-[45%] -top-[60px]">
-                    <Logo />
-                </span>
-              
+            <div className='lg:w-[300px] w-[60%] text-center relative'>
+
+                <div className='w-full flex justify-center'>
+                    <Image
+                        src={logo}
+                        className="w-[150px] h-[150px]"
+                        alt="Proactive Ideas Logo"
+                     
+                    />
+                </div>
+
                 <div className="linez">
                     <div className="inner"></div>
                 </div>
