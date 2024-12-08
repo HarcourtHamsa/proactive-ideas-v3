2e48837cf41388c215dc877e5e2baf61895e2b39
Harcourt Hamsa <hamsaharcourt@gmail.com>
2024-12-08 03:45:36 -0800

    Update http.ts

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const http = axios.create({
   // baseURL: "http://localhost:8000/api/",
-  baseURL: "https://shielded-cliffs-15232-110fd52a9c2c.herokuapp.com/api/",
+  baseURL: "https://proactive-ideas-backup-6ef9825a1d26.herokuapp.com/api/",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "X-Requested-With, Content-Type",
